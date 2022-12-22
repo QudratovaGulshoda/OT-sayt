c591c3c28658f13cdccb30f80da281d678374fc9
Gulshoda <samiraqudratova809@gmail.com>
2022-12-22 20:07:00 +0500

    sayt

--- app1/urls.py
+++ app1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
+    
+    path('windows/',page11),
+    path('macos/',page22),
+    path('linux/',page33),
+    path('andiroid/',page44),
+    path('ios/',page55),
     path('',home),
-    path('1',page11),
-    path('2',page22),
-    path('3',page33),
-    path('4',page44),
-    path('5',page55),
 ]
